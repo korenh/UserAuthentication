a02a8bbb59439b4a8a7adee750cb0edf4a002545
korenh <korenx123@gmail.com>
2021-03-21 02:22:50 +0200

    add bcrypt

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 const Client = require('mongodb').MongoClient;
+const config = require('./config.json')
 const express = require("express");
+const bcrypt = require('bcrypt');
 const cors = require('cors')
 const app = express();
-const config = require('./config.json')
 app.use(express.json());
 app.use(cors())
 
 app.post('/register',(req,res) => {
-    console.log(req.data)
     Client.connect(config.URL ,  { useUnifiedTopology: true } ,(err, db) => {
         if (err) throw err;
+        bcrypt.hash(req.body.password, 10 , function(err, password) {
         db.db(config.DB).collection(config.COLL).insertOne({
             name:req.body.name,
             email:req.body.email,
-            password:req.body.password
+            password:password
         }
         ,(err, res) => {
           if (err) throw err;
           db.close();
-        });
+        })})
         res.status(200).json(config.SUCCESS)
     });
 });
@@ -27,12 +28,12 @@ app.post('/login',(req,res) => {
     Client.connect(config.URL , { useUnifiedTopology: true },(err, db) => {
         if (err) throw err;
         db.db(config.DB).collection(config.COLL).find({email:req.body.email}).toArray((err, result) => {
-            if (err) throw err;
             db.close();
-            result.length!==0?
-            result[0].password === req.body.password ? res.status(200).json(result[0]) 
-            :res.status(401).json(config.FAIL[0])
-            :res.status(401).json(config.FAIL[1])
+            if (err) throw err;
+            result.length===0? res.status(401).json(config.FAIL[1]):
+            bcrypt.compare(req.body.password, result[0].password , function(err, bcryptStatus) {
+                bcryptStatus?res.status(200).json(result[0]):res.status(401).json(config.FAIL[0])
+            });
         });
     });
 });
